2fdb3e01ea276317459ebfa2e8dbe16a52715075
speeed76 <pawel.giers@gmail.com>
2023-08-26 12:51:55 +0000

    changed to deployable

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -16,8 +16,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Pawel!, latest update after setting up github.")
+}
+
 func main() {
-	fmt.Println("Hello, Pawel!, latest update after setting up github.")
+	http.HandleFunc("/", handler)
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
